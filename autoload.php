bc823d0b6d802b6e39f90282ac6a55c7cdfc8461
TaroMori0321 <87011031+TaroMori0321@users.noreply.github.com>
2022-05-11 12:21:56 +0900

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -12,13 +12,13 @@ spl_autoload_register(function (string $classname): bool {
     foreach (AUTOLOAD_BASE_DIRECTORIES as $namespace => $directory) {
         // クラスの完全修飾名が、$namespaceと前方一致するか
         if (strpos($classname, $namespace) === 0) {
+            // クラスの完全修飾名から名前空間を取り除いた文字列を取得
             $classname_after = ltrim($classname, $namespace);
-            $filename = $directory . join('/', explode('\\', $classname_after)) . '.php';
 
-            if (is_file($filename) && is_readable($filename)) {
-                require_once $filename;
-                return true;
-            }
+            // クラスファイルのパスを組み立てて読み込む
+            require_once $directory . join('/', explode('\\', $classname_after)) . '.php';
+            
+            return true;
         }
     }
     return false;
