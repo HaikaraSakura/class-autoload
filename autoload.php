38a3f23d0bd2e4da5f143a1b3b74d161654f7601
TaroMori0321 <87011031+TaroMori0321@users.noreply.github.com>
2022-05-11 11:45:34 +0900

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -8,14 +8,14 @@ define('AUTOLOAD_BASE_DIRECTORIES', [
 ]);
 
 // オートローダを登録
-spl_autoload_register(function (string $class_fullname): bool {
+spl_autoload_register(function (string $classname): bool {
     if (defined('AUTOLOAD_BASE_DIRECTORIES')) {
         foreach (AUTOLOAD_BASE_DIRECTORIES as $namespace => $directory) {
-            $before_namespace = ltrim($class_fullname, $namespace);
-            $file_name = $directory . join('/', explode('\\', $before_namespace)) . '.php';
+            $classname_after = ltrim($classname, $namespace);
+            $filename = $directory . join('/', explode('\\', $classname_after)) . '.php';
 
-            if (file_exists($file_name) && is_readable($file_name)) {
-                require_once $file_name;
+            if (file_exists($filename) && is_readable($filename)) {
+                require_once $filename;
                 return true;
             }
         }
